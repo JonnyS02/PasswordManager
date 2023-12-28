87fabba1ff851c21a8e488a02d5fcc9017d6f902
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2023-12-28 23:58:21 +0100

    Email verification

--- app/Views/verified.php
+++ app/Views/verified.php
@@ -20,7 +20,7 @@ include "partials/header.php";
     <div class="centered-div">
         <img src="<?= base_url('assets/verified.gif') ?>" alt="GIF" id="gif">
         <img src="<?= base_url('assets/verified.png') ?>" alt="Last Frame" id="lastFrame" class="hidden">
-        <hr style="margin-left: calc(50% - 220px);width: 400px">
+        <hr style="margin-left: calc(50% - 210px);width: 420px">
         <?php if (isset($main) == 1){ echo '<p style="font-size: 20px; text-align: center">Your email address has been verified.<br>You can now use your PassSafePro Account.</p>';
             echo '<a href="' . base_url() . '" class="btn btn-primary">Back to login</a>';}
         ?>
@@ -35,7 +35,7 @@ include "partials/header.php";
         setTimeout(function () {
             document.getElementById('gif').classList.add('hidden');
             document.getElementById('lastFrame').classList.remove('hidden');
-        }, 3200);
+        }, 3250);
     });
 </script>
 
