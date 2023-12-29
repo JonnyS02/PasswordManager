b87aa024410308d96c7f9bfd28c72a3f1f05fd52
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2023-12-29 19:08:54 +0100

    Font fix

--- app/Views/partials/head.php
+++ app/Views/partials/head.php
@@ -4,6 +4,8 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
     <link href="<?php echo base_url("assets/style.css")?>" rel="stylesheet"/>
     <link href="<?php echo base_url("assets/modal_style.css")?>" rel="stylesheet"/>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Smooch+Sans">
     <link rel="icon" type="image/x-icon" href="<?= base_url('assets/icon.ico') ?>">
     <title>PassSafe Pro: Easy, Simple, Independent!</title>
 </head>
