87df26a9ac837c09eebef07b56564f3a800a9063
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-01-04 20:48:05 +0100

    Minor bug fixes

--- app/Views/mailTemplate.php
+++ app/Views/mailTemplate.php
@@ -3,8 +3,6 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Dancing+Script">
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Smooch+Sans">
     <title>PassSafe Pro</title>
 </head>
 <body style="font-family: 'Arial', sans-serif; background-color: #f4f4f4; margin: 0; padding: 0;">
