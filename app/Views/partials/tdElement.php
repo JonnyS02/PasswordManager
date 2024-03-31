fb90f50b818d87a7e1bb210f8f2fbb41144bd8c7
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-03-31 16:14:57 +0200

    Responsive style improvement

--- app/Views/partials/tdElement.php
+++ app/Views/partials/tdElement.php
@@ -1,4 +1,5 @@
 <td> <span style="cursor: pointer;"
+           title="<?= $passwordFormList[$tdContent] ?>"
            onmouseover="this.style.color='#00bcff'"
            onmouseout="this.style.color='black'"
            onclick="copy('<?= $passwordFormList[$tdContent] ?>')"><?= $passwordFormList[$tdContent] ?></span>
