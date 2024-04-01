4b6b5187ead407e8fe262f247272f8a364afa184
JonnyS02 <32039961+JonnyS02@users.noreply.github.com>
2024-04-01 19:48:09 +0200

    Overall improvement

--- app/Views/partials/footer.php
+++ app/Views/partials/footer.php
@@ -1,7 +1,6 @@
 <?php include_once(dirname(__FILE__) . '/../../../Config.php'); ?>
-<div id="footer"
-     style=" background-color: #333;color: white;padding: 10px;text-align: center;">
-    © <?= date("Y")." ".getenv('Admin') ?>
+<div id="footer" style=" background-color: #333;color: white;padding: 10px;text-align: center;">
+    © <?= date("Y") . " " . getenv('Admin') ?>
     <a href="https://github.com/JonnyS02/PasswordManager">
         <i class="fab fa-github" style="color: white;"></i></a>
 </div>
